0497f374a0b302d68461cb964a62924433942812
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2020-03-25 13:09:41 +0100

    Update teclado.c

--- source/teclado.c
+++ source/teclado.c
@@ -15,6 +15,29 @@ int  TeclaPulsada() {
    // A=0; B=1; Select=2; Start=3; Der=4; Izq=5;
    // Arriba=6; Abajo=7; R=8; L=9;
    // -1 en otros casos
+   int tecla = (~TECLAS_DAT & 0x03FF);
+        switch (tecla)
+        {       case 1:
+                        return A;
+                case 2:
+                        return B;
+                case 4:
+                        return SELECT;
+                case 8:
+                        return START;
+                case 16:
+                        return DCHA;
+                case 32:
+                        return IZDA;
+                case 64:
+                        return ARRIBA;
+                case 128:
+                        return ABAJO;
+                case 256:
+                        return R;
+                case 512:
+                        return L;
+        }
 
 }
 
